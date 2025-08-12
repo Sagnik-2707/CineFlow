28c5c877301b328495a46b030b88e0623515becd
Sagnik-2707 <sagnikmanna537@gmail.com>
2025-08-12 12:33:22 +0530

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="footer-class">Footer</div>
+    <div className="footer-class">
+    <div className="first-column">
+        <p>About</p>
+        <p>Contact Us</p>
+        <p>FAQ</p>
+        <p>Help Center</p>
+    </div>
+    <div className="second-column">
+        <p>Investor Relations</p>
+        <p>Speed Test</p>
+        <p>API</p>
+        <p>Corporate Information</p>
+    </div>
+    <div className="third column">
+        <p>Privacy</p>
+        <p>Legal Notices</p>
+        <p>Cookie Preferences</p>
+        <p>Ways to Watch</p>
+    </div>
+    </div>
   )
 }
 
